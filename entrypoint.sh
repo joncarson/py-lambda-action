4208302252070eb58890d0d1c2e86e5c7bbf5546
Jon Carson <jonathan.h.carson@gmail.com>
2021-03-28 17:50:14 -0400

    string compare

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ publish_function_code(){
 	echo "Deploying the code for directory: ${1::-1}"
 	zip -r -j ${1::-1}.zip $1* -x \*.git\*
 
-	if ["${1:0:1}" = "z"]; then
+	if [ "${1:0:1}" = "z" ]; then
 
 		for i in {0..499}
 		do
@@ -39,7 +39,7 @@ publish_function_code(){
 
 update_function_layers(){
 	echo "Using the layer in the function..."
-	if ["${1:0:1}" = "z"]; then
+	if [ "${1:0:1}" = "z" ]; then
 		for i in {0..499}
 		do
 			function_number="00$i"
